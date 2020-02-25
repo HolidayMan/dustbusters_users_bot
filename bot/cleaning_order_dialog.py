02af6c1f4a2ac754201fa6e1d9dca3867f08c47c
HolidayMan <lehafirstchanel@gmail.com>
2020-02-25 13:01:27 +0200

    template for handlers cleaning dialogue

--- bot/cleaning_order_dialog.py
+++ bot/cleaning_order_dialog.py
@@ -1,4 +1,32 @@
 from .bot import bot
 from .keyboards import *
+import bot.phrases as ph
+
+def handle_cleaning_type(message):
+    pass
 
 
+def handle_place_size(message):
+    pass
+
+
+def handle_time_range(message):
+    pass
+
+
+def handle_cleaning_date(message):
+    pass
+
+
+def handle_cleaning_time(message):
+    pass
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_need_additional_service(message):
+    pass
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_additional_sevices(message):
+    pass

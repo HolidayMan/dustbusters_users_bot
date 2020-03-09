68c197c5529710d8004adea13c73a0b43a8d4f1b
HolidayMan <lesha.pitenko791@gmail.com>
2020-03-10 00:03:51 +0200

    added time handling added cancel for additional service

--- bot/cleaning_order_dialog.py
+++ bot/cleaning_order_dialog.py
@@ -1,12 +1,14 @@
 import re
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 from .bot import bot
 from .keyboards import *
 import bot.phrases as ph
-from .handlers import handle_back_to_menu
+from .handlers import handle_back_to_menu, back_to_menu_handler
 from .models import CleaningOrder
-from .utils import get_keyboards_buttons_text, get_last_db_obj, get_trip_type_from_name, has_message_text
+from .utils import (get_keyboards_buttons_text, get_last_db_obj, get_trip_type_from_name,
+                    has_message_text, set_state, set_menu_state, get_current_state)
+from .states import States
 
 
 def get_tomorrow_date() -> datetime:
@@ -67,7 +69,7 @@ def handle_time_range(message):
 
 @handle_back_to_menu(delete=True, model=CleaningOrder)
 def handle_cleaning_date(message):
-    pattern = r'^([0-2][1-9]|3[0-1])\.(0[1-9]|[1-2][0-2])\.2\d\d\d$'
+    pattern = r'^(0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|[1-2][0-2])\.2\d\d\d$'
     if not has_message_text(message) or not re.match(pattern, message.text):
         bot.register_next_step_handler(message, handle_cleaning_date)
         return bot.send_message(message.chat.id, ph.INVALID_DATE, parse_mode="HTML")
@@ -75,7 +77,7 @@ def handle_cleaning_date(message):
     order_date = datetime.strptime(message.text, "%d.%m.%Y").date()
     today = (datetime.utcnow() + timedelta(hours=3)).date()
 
-    if not today < order_date:
+    if not today <= order_date:
         bot.register_next_step_handler(message, handle_cleaning_date)
         return bot.send_message(message.chat.id, ph.PAST_DATE, parse_mode="HTML")
 
@@ -90,7 +92,7 @@ def handle_cleaning_date(message):
     elif order.trip == CleaningOrder.NIGHT_TRIP:
         time_to_format = "04:00"
 
-    bot.register_next_step_handler(message, handle_cleaning_date)
+    bot.register_next_step_handler(message, handle_cleaning_time)
     return (bot.send_message(message.chat.id, ph.SHOW_PRICE % order.calc_price(), parse_mode="HTML"),
             bot.send_message(message.chat.id, ph.ENTER_TIME % time_to_format,
                              parse_mode="HTML", reply_markup=BACK_TO_MENU_KEYBOARD))
@@ -98,13 +100,53 @@ def handle_cleaning_date(message):
 
 @handle_back_to_menu(delete=True, model=CleaningOrder)
 def handle_cleaning_time(message):
+    pattern = r"([0-1]*[0-9]|2[0-3]):([0-5][0-9])"
+    if not has_message_text(message) or not re.match(pattern, message.text):
+        bot.register_next_step_handler(message, handle_cleaning_time)
+        return bot.send_message(message.chat.id, ph.INVALID_TIME, parse_mode="HTML")
+
+    order_time = datetime.strptime(message.text, "%H:%M").time()
+
+    order = get_last_db_obj(model=CleaningOrder, user=message.chat)
+
+    if order.trip == CleaningOrder.DAY_TRIP:
+        if not time(hour=9) <= order_time < time(hour=16):
+            bot.register_next_step_handler(message, handle_cleaning_time)
+            return bot.send_message(message.chat.id, ph.TIME_MUST_BE_DAY, parse_mode="HTML")
+    elif order.trip == CleaningOrder.EVENING_TRIP:
+        if not time(hour=16) <= order_time < time(hour=22):
+            bot.register_next_step_handler(message, handle_cleaning_time)
+            return bot.send_message(message.chat.id, ph.TIME_MUST_BE_EVENING, parse_mode="HTML")
+    elif order.trip == CleaningOrder.NIGHT_TRIP:
+        if not (time(hour=22) <= order_time <= time(hour=23, minute=59) or time(hour=0) <= order_time <= time(hour=9)):
+            bot.register_next_step_handler(message, handle_cleaning_time)
+            return bot.send_message(message.chat.id, ph.TIME_MUST_BE_NIGHT, parse_mode="HTML")
+
+    order.time = order_time
+    order.save()
+
+    set_state(message.chat.id, States.S_HANDLE_ADDITIONAL_SERVICE)
+    return (bot.send_message(message.chat.id, ph.SHOW_PRICE % order.calc_price(), parse_mode="HTML", reply_markup=deleteKeyboard),
+            bot.send_message(message.chat.id, ph.CHOOSE_ADDITIONAL_SERVICE,
+                             parse_mode="HTML", reply_markup=ADDSERVICE_KEYBOARD))
+
+
+@bot.callback_query_handler(func=lambda call: get_current_state(user_id=call.message.chat.id) and call.data == YES_ADDSERVICE_BUTTON.callback_data)
+def handle_additional_services_yes(call):
     pass
 
 
-@bot.callback_query_handler(func=lambda call: True)
-@handle_back_to_menu
-def handle_additional_services(message):
+@bot.callback_query_handler(func=lambda call: get_current_state(user_id=call.message.chat.id) and call.data == NO_ADDSERVICE_BUTTON.callback_data)
+def handle_additional_services_no(call):
     pass
+
+
+@bot.callback_query_handler(func=lambda call: get_current_state(user_id=call.message.chat.id) and call.data == ADDSERVICE_BACK_TO_MENU_BUTTON.callback_data)
+def handle_additional_services_cancel(call):
+    order = get_last_db_obj(model=CleaningOrder, user=call.message.chat)
+    order.delete()
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+    return back_to_menu_handler(call.message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -10,8 +10,15 @@ DAY_TRIP_BUTTON = types.KeyboardButton("Дневной выезд 9:00 — 15:00
 EVENING_TRIP_BUTTON = types.KeyboardButton("Вечерний выезд 16:00 — 21:00 (1000 ₽)")
 NIGHT_TRIP_BUTTON = types.KeyboardButton("Ночной выезд 22:00 — 8:00 (2000 ₽)")
 
+YES_ADDSERVICE_BUTTON = types.InlineKeyboardButton("Да✅", callback_data="additional_service_accepted")
+NO_ADDSERVICE_BUTTON = types.InlineKeyboardButton("Нет🚫", callback_data="additional_service_declined")
+ADDSERVICE_BACK_TO_MENU_BUTTON = types.InlineKeyboardButton("❌", callback_data="cleaning_cancel")
+
 BACK_TO_MENU_BUTTON = types.KeyboardButton("Назад")
 deleteKeyboard = types.ReplyKeyboardRemove()
+
+BACK_TO_MENU_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+BACK_TO_MENU_KEYBOARD.add(BACK_TO_MENU_BUTTON)
 
 AUTHORIZE_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
 AUTHORIZE_KEYBOARD.add(AUTHORIZE_BUTTON)
@@ -24,8 +31,8 @@ CLEANING_TYPE_KEYBOARD.add(CLEANING_WITHOUT_WINDOWS, CLEANING_WITH_WINDOWS, BACK
 PLACE_SIZE_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
 PLACE_SIZE_KEYBOARD.add(BACK_TO_MENU_BUTTON)
 
-TIME_RANGE_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+TIME_RANGE_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
 TIME_RANGE_KEYBOARD.add(DAY_TRIP_BUTTON, EVENING_TRIP_BUTTON, NIGHT_TRIP_BUTTON, BACK_TO_MENU_BUTTON)
 
-BACK_TO_MENU_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-BACK_TO_MENU_KEYBOARD.add(BACK_TO_MENU_BUTTON)
+ADDSERVICE_KEYBOARD = types.InlineKeyboardMarkup(row_width=2)
+ADDSERVICE_KEYBOARD.add(YES_ADDSERVICE_BUTTON, NO_ADDSERVICE_BUTTON, ADDSERVICE_BACK_TO_MENU_BUTTON)
